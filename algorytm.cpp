e34c7cf8b68a0e19dab55505721155f5ea816120
PlaneGG <pawel.dietrich@gmail.com>
2017-01-06 22:16:08 +0100

    New Code

--- algorytm.cpp
+++ algorytm.cpp
@@ -15,20 +15,27 @@ Styczen 2017 (c)
 
 using namespace std;
 
-int round_sqrt(int liczba){
-    int upper=46341,lower=0; // upper to pierwiastek zaokraglony w gore od maksymalnej wartosci int.
-    while (upper != lower)
-    {
-        int m = (upper+lower)/2;
-        if (m*m >= liczba)
-          upper  = m; // szukaj od m+1 do b
-        else
-          lower = m+1;   // szukaj od a do m
-    }
-    return upper;
+int sumVectorElements(vector < int > input){
+    int sum;
+    for(int i=0;i<input.size();i++)
+        sum += input[i];
+    return sum;
 }
 
-vector < int > sito_eratostenesa(int maksimum){
+//int roundSqrt(int liczba){
+//    int upper=46341,lower=0; // upper to pierwiastek zaokraglony w gore od maksymalnej wartosci int.
+//    while (upper != lower)
+//    {
+//        int m = (upper+lower)/2;
+//        if (m*m >= liczba)
+//          upper  = m;
+//        else
+//          lower = m+1;
+//    }
+//    return upper;
+//}
+
+vector < int > sitoEratostenesa(int maksimum){
     vector < bool > liczby (maksimum+5);
     vector < int > odp;
     for(int i=2;i<=maksimum;i++){
@@ -41,15 +48,47 @@ vector < int > sito_eratostenesa(int maksimum){
     return odp;
 }
 
+vector <int> rozklad (int liczba)
+{
+    vector < int > czynniki;
+    vector < int > pierwsze = sitoEratostenesa(liczba);
+    int d=0;
+    while(liczba>1){
+        if (liczba%pierwsze[d]==0){
+            czynniki.push_back(pierwsze[d]);
+            liczba=liczba/pierwsze[d];
+        }
+        else
+            d=d+1;
+    }
+    return czynniki;
+}
 
-
-
-using namespace std;
+vector <int> dzielniki (vector <int> rozklad)
+{
+    int s=0;
+    vector < int > skladniki;
+    vector < int > liczbaSkl;
+    skladniki.push_back(rozklad[0]);
+    liczbaSkl.push_back(1);
+    for(int i=1;i<rozklad.size();i++){
+        if(rozklad[i]==rozklad[i-1]){
+                liczbaSkl[s]++;
+        }else{
+            skladniki.push_back(rozklad[i]);
+            liczbaSkl.push_back(1);
+            s++;
+        }
+    }
+    vector < int > odp;
+    //WIP
+    return odp;
+}
 
 int main(){
     int a;
     cin >> a;
-
+    dzielniki(rozklad(a));
 
 
 
